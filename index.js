f82c1ccec8642bb983d5dc7fd132f5be8cefb60e
jbensimon <bensimon.jeremy@gmail.com>
2016-05-02 17:48:09 +0200

    file or glob required as first arg, remove json option : use second arg, render all files before watch

--- index.js
+++ index.js
@@ -7,46 +7,39 @@ var chokidar = require('chokidar')
 var mkdirp = require('mkdirp')
 var chalk = require('chalk')
 var yargs = require('yargs')
-  .usage('Usage: nj [file] [context] [options]')
-  .example('nj foo.tpl -o dist', 'Render foo.tpl to dist/foo.html')
-  .example('nj -w -p src -o dist -j data.json',
-    'Watch .tpl files in src folder, render them to dist folder, with the context of data.json')
-  .option('watch',{
-    alias: 'w',
-    boolean: true,
-    skipValidation: true,
-    describe: 'Watch and render .tpl files, except the ones starting by "_"'
-  })
+  .usage('Usage: nj <file|*.ext> [context] [options]')
+  .example('nj foo.tpl data.json', 'Compile foo.tpl to dist/foo.html')
+  .example('nj *.tpl -w -p src -o dist',
+    'Watch .tpl files in src folder, compile them to dist folder')
+  .demand(1)
   .option('path', {
     alias: 'p',
     string: true,
     requiresArg: true,
     nargs: 1,
-    describe: 'Templates path'
+    describe: 'Path where templates live'
   })
   .option('out', {
     alias: 'o',
     string: true,
     requiresArg: true,
     nargs: 1,
-    describe: 'Destination folder'
+    describe: 'Output folder'
   })
-  .option('json', {
-    alias: 'j',
-    string: true,
-    requiresArg: true,
-    nargs: 1,
-    describe: 'Load JSON context file'
+  .option('watch',{
+    alias: 'w',
+    boolean: true,
+    describe: 'Watch files change, except files starting by "_"'
   })
   .option('options', {
     alias: 'O',
     string: true,
     requiresArg: true,
     nargs: 1,
-    describe: 'Nunjucks option file'
+    describe: 'Nunjucks options file'
   })
-  .help('h')
-  .alias('h', 'help')
+  .help()
+  .alias('help', 'h')
   .epilogue('For more information on Nunjucks: https://mozilla.github.io/nunjucks/api.html')
   
 var argv = yargs.argv
@@ -59,38 +52,42 @@ opts.nunjucks = argv.options || {
   noCache: true
 }
 var env = nunjucks.configure(opts.dirIn, opts.nunjucks)
-var context = (argv.j) ? JSON.parse(fs.readFileSync(argv.j, 'utf8')) : (argv._[1]) ? JSON.parse(fs.readFileSync(argv._[1], 'utf8')) : {}
+var context = (argv._[1]) ? JSON.parse(fs.readFileSync(argv._[1], 'utf8')) : {}
 
-if (!argv._[0] && !argv.watch) yargs.showHelp("log")
-if (argv._[0]) render(argv._[0], context)
+if (argv._[0].indexOf('*') === 0) {
+  var allFiles = walkSync(opts.dirIn, path.extname(argv._[0]))
+  opts.glob = './**/*' + path.extname(argv._[0])
+  renderAll(allFiles, context)
+} else {
+  opts.glob = argv._[0]
+  render(argv._[0], context)
+}
+
 if (argv.watch) {
-  opts.glob = './**/*.tpl'
   opts.chokidar = {
     persistent: true,
     cwd: opts.dirIn
   }
   var watcher = chokidar.watch(opts.glob, opts.chokidar)
   var layouts = []
-  var files = []
+  var templates = []
 
   watcher.on('ready', function() {
     console.log(chalk.gray('Watching templates...'))
   })
 
-  watcher.on('add', function(file, details) {
+  watcher.on('add', function(file) {
     if (path.basename(file).indexOf('_') === 0) {
       layouts.push(file)
     } else {
-      files.push(file)
+      templates.push(file)
     }
   })
 
   watcher.on('change', function(file) {
     if (layouts.indexOf(file) > -1) {
-      console.log(chalk.cyan('Rendering: ' + files))
-      renderAll(files, context)
+      renderAll(templates, context)
     } else {
-      console.log(chalk.blue('Rendering: ' + file))
       render(file, context)
     }
   })
@@ -104,6 +101,7 @@ function render(file, data) {
       output = opts.dirOut + '\\' + output
       mkdirp.sync(path.dirname(output))
     }
+    console.log(chalk.blue('Rendering: ' + file))
     fs.writeFileSync(output, res)
   })
 }
@@ -112,4 +110,19 @@ function renderAll(files, data) {
   for (var i = 0; i < files.length; i++) {
     render(files[i], data)
   }
+}
+
+function walkSync(dir, ext, filelist) {
+  dir = dir || '.'
+  dir += '/'
+  filelist = filelist || []
+  var files = fs.readdirSync(dir)
+  files.forEach(function(file) {
+    if (fs.statSync(dir + file).isDirectory()) {
+      filelist = walkSync(dir + file + '/', ext, filelist)
+    } else {
+      if (path.extname(file) === ext && path.basename(file).indexOf('_') !== 0) filelist.push(file)
+    }
+  })
+  return filelist
 }
--- readme.md
+++ readme.md
@@ -1,10 +1,15 @@
 # Nunjucks-cli
 Simple Nunjucks CLI Wrapper and templates watcher, to generate static HTML files.
+## Installation
+    npm i -g nunjucks-cli
 ## Usage
-    nunjucks [file] [context] [options]
-#### Basic example with one file
-    nunjucks foo.tpl bar.json
-Compiles foo.tpl to foo.html with data from bar.json.
+    nunjucks <file|*.ext> [context] [options]
+#### Basic examples
+    nunjucks foo.tpl data.json
+Compiles `foo.tpl` to `foo.html` with data from `data.json`.
+
+    nunjucks *.tpl
+Compiles all `.tpl` files, except the ones starting by `_`.
 
 ## Options
 ### `--path <directory>`
@@ -18,19 +23,22 @@ See https://mozilla.github.io/nunjucks/api.html#configure
 
 Output directory.
 
+### `--watch`
+`-w`
+
+Allows to keep track of file changes and render accordingly (expect files starting by `_`).
+
 ### `--options <file>`
 `-O <file>`
 
 Takes a json file as Nunjucks options.
 See https://mozilla.github.io/nunjucks/api.html#configure
 
-#### Advanced example with one file
+#### Advanced examples
 
-    nunjucks foo.tpl -p ./src -o ./dist -O nj.json
-Compiles src/foo.tpl to dist/foo.html, with nj.json as nunjucks environnement options.
+    nunjucks foo.tpl -p src -o dist -O nj.json
+Compiles `src/foo.tpl` to `dist/foo.html`, with `nj.json` as nunjucks environnement options.
 
-## Watcher
-### `--watch` / `-w`
-Run a watcher in the background which renders .tpl files.
+    nunjucks *.tpl data.json -w -p src
+Compiles all `.tpl` files (except ones starting with `_`) in the `src` folder to the current working directory, with `data.json` as metadata, and keeps running in the background for files changes.
 
-`nunjucks -w` : Watch and render .tpl files in the current working directory, except the ones starting by "_".
